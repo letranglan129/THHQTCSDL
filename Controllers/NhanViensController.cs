6ebdc9e1c3d87bb0bfd5d41be05bfc9a1eed248d
LeLan <letranglan129@gmail.com>
2022-12-09 20:20:31 +0700

    first commit

---
+++ Controllers/NhanViensController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QLST.Models;
+
+namespace QLST.Controllers
+{
+    public class NhanViensController : Controller
+    {
+        private QLSTEntities db = new QLSTEntities();
+
+        // GET: NhanViens
+        public ActionResult Index()
+        {
+            return View(db.NHANVIENs.ToList());
+        }
+
+        // GET: NhanViens/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // GET: NhanViens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NhanViens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MANV,TENNV,CHUCVU,NGAYCONG,LUONGCOBAN,TONGLUONG")] NHANVIEN nHANVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NHANVIENs.Add(nHANVIEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nHANVIEN);
+        }
+
+        // GET: NhanViens/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // POST: NhanViens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MANV,TENNV,CHUCVU,NGAYCONG,LUONGCOBAN,TONGLUONG")] NHANVIEN nHANVIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nHANVIEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nHANVIEN);
+        }
+
+        // GET: NhanViens/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            if (nHANVIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nHANVIEN);
+        }
+
+        // POST: NhanViens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NHANVIEN nHANVIEN = db.NHANVIENs.Find(id);
+            db.NHANVIENs.Remove(nHANVIEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QLST.MvcApplication" Language="C#" %>
