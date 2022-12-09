55c4f1c5e8c481400c2f7f46eec82aa131ee7760
kenhvit123 <100826952+kenhvit123@users.noreply.github.com>
2022-12-10 00:15:01 +0700

    doi route

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace QLST
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "NhanViens", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
